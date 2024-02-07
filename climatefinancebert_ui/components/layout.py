7aa8da331e690528d9875ff6dece25671642eabb
Oliver Guggenbühl <oliver.guggenbuehl@statworx.com>
2024-02-07 01:27:54 +0100

    add sidebar to layout
    minor

--- climatefinancebert_ui/components/layout.py
+++ climatefinancebert_ui/components/layout.py
@@ -9,72 +9,82 @@ from climatefinancebert_ui.components import (
     year_slider,
 )
 
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "24rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+
+def build_sidebar(app: Dash) -> html.Div:
+    return html.Div(
+        [
+            html.H3("Filters"),
+            html.Hr(),
+            html.P("Mix and match your selection to filter the data"),
+            dbc.Nav(
+                [
+                    type_dropdown.render(app),
+                    html.Br(),
+                    categories_dropdown.render(app),
+                    html.Br(),
+                    year_slider.render(app),
+                ],
+                vertical=True,
+                pills=True,
+            ),
+        ],
+        style=SIDEBAR_STYLE,
+    )
+
 
 def create_layout(app: Dash) -> html.Div:
     return dbc.Container(
         className="app-div",
         children=[
-            dbc.Row(
-                dbc.Col(
-                    html.H1(app.title),
-                )
+            dbc.Col(
+                build_sidebar(app),
+                width=3,
             ),
-            html.Hr(),
-            dbc.Row(
-                dbc.Col(
-                    html.Div(
-                        className="map-container",
-                        children=[
-                            map.render(
-                                app,
-                            )
-                        ],
-                    )
-                )
-            ),
-            html.Br(),
-            dbc.Row(
+            dbc.Col(
                 [
-                    dbc.Col(
-                        html.Div(
-                            className="dropdown-container",
-                            children=[
-                                year_slider.render(app),
-                            ],
-                        ),
-                        width=5,
+                    dbc.Row(
+                        dbc.Col(
+                            html.H1(app.title),
+                        )
                     ),
-                    dbc.Col(
-                        html.Div(
-                            className="dropdown-container",
-                            children=[
-                                type_dropdown.render(app),
-                            ],
-                        ),
-                        width=2,
+                    html.Hr(),
+                    dbc.Row(
+                        dbc.Col(
+                            html.Div(
+                                className="map-container",
+                                children=[
+                                    map.render(
+                                        app,
+                                    )
+                                ],
+                            ),
+                            width=12,
+                        )
                     ),
-                    dbc.Col(
-                        html.Div(
-                            className="dropdown-container",
-                            children=[
-                                categories_dropdown.render(app),
-                            ],
-                        ),
-                        width=5,
+                    html.Br(),
+                    dbc.Row(
+                        dbc.Col(
+                            html.Div(
+                                className="datatable-container",
+                                children=[
+                                    datatable.render(app),
+                                ],
+                            ),
+                            width=12,
+                        )
                     ),
-                ]
-            ),
-            html.Br(),
-            dbc.Row(
-                dbc.Col(
-                    html.Div(
-                        className="datatable-container",
-                        children=[
-                            datatable.render(app),
-                        ],
-                    ),
-                    width=12,
-                )
+                ],
+                width={"size": 9, "offset": 3},
             ),
         ],
     )
