f1c041af38d80e0e733dc164c1b0a37023df3fac
Oliver Guggenbühl <oliver.guggenbuehl@statworx.com>
2024-03-22 15:36:04 +0100

    get coloring to run - still buggy

--- climatefinancebert_ui/callbacks/map_callbacks.py
+++ climatefinancebert_ui/callbacks/map_callbacks.py
@@ -1,10 +1,35 @@
 import dash_leaflet as dl
 import pandas as pd
 from dash import Input, Output, State, dash
-from dash_extensions.javascript import arrow_function
+from dash_extensions.javascript import arrow_function, assign
 
 from climatefinancebert_ui.components import constants, ids, infobox_country, reset_button
 from climatefinancebert_ui.components.utils import merge_data, prepare_data_for_merge
+
+classes = [0, 10, 20, 50, 100, 200, 500, 1000]
+colorscale = [
+    "#FFEDA0",
+    "#FED976",
+    "#FEB24C",
+    "#FD8D3C",
+    "#FC4E2A",
+    "#E31A1C",
+    "#BD0026",
+    "#800026",
+]
+style = dict(weight=2, opacity=1, color="white", dashArray="3", fillOpacity=0.7)
+
+# Geojson rendering logic, must be JavaScript as it is executed in clientside.
+style_handle = assign("""function(feature, context){
+    const {classes, colorscale, style, polyColoring} = context.hideout;  // get props from hideout
+    const value = feature.properties[polyColoring];  // get value the determines the color
+    for (let i = 0; i < classes.length; ++i) {
+        if (value > classes[i]) {
+            style.fillColor = colorscale[i];  // set the fill color according to the class
+        }
+    }
+    return style;
+}""")
 
 
 def register(app):
@@ -54,7 +79,13 @@ def register(app):
             dl.GeoJSON(
                 id=ids.COUNTRIES_LAYER,
                 data=stored_geojson,
-                hoverStyle=arrow_function(dict(weight=4, color="#666", dashArray="")),
+                style=style_handle,  # how to style each polygon
+                hoverStyle=arrow_function(
+                    dict(weight=5, color="#666", dashArray="")
+                ),  # style applied on hover
+                hideout=dict(
+                    colorscale=colorscale, classes=classes, style=style, polyColoring="value"
+                ),
                 zoomToBoundsOnClick=True,
                 interactive=True,
             ),
